d46918174577cbecb3469ba4df880e5711ac3c98
Kai Kuhfeld <kaikuhfeld@aol.com>
2017-12-22 13:18:00 +0100

    PDFLocations final

--- kreditantrag/src/main/java/MASWF/kreditantrag/erstelleKreditvertrag.java
+++ kreditantrag/src/main/java/MASWF/kreditantrag/erstelleKreditvertrag.java
@@ -10,17 +10,18 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class erstelleKreditvertrag implements JavaDelegate {
-
+	private final String PDFLocation = "C:/Users/gelbe/git/MASBKS/kreditantrag/src/main/resources/formtest.pdf";
+	private final String PDFSpeichern = "C:/Users/gelbe/Desktop/formfilled.pdf";
 	@Override
 	public void execute(DelegateExecution exec) throws Exception {
-		PDDocument pdfdoc = PDDocument.load(new File("C:/Users/gelbe/git/MASBKS/kreditantrag/src/main/resources/formtest.pdf"));
+		PDDocument pdfdoc = PDDocument.load(new File(PDFLocation));
 		PDDocumentCatalog cat = pdfdoc.getDocumentCatalog();
 		PDAcroForm acro = cat.getAcroForm();
 		if (acro != null) {
 			PDField field = (PDField) acro.getField("vorname");
 			field.setValue(exec.getVariable("vorname").toString());
 		}
-		pdfdoc.save(new File("C:/Users/gelbe/Desktop/formfilled.pdf"));
+		pdfdoc.save(new File(PDFSpeichern));
 		pdfdoc.close();
 	}
 
