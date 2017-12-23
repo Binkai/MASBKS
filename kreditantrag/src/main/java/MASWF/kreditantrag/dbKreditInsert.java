a76d33da238ef7919784d9dff29580cdc5fce146
Kai Kuhfeld <kaikuhfeld@aol.com>
2017-12-23 11:58:39 +0100

    Alles funktionsfähig...

--- kreditantrag/src/main/java/MASWF/kreditantrag/dbKreditInsert.java
+++ kreditantrag/src/main/java/MASWF/kreditantrag/dbKreditInsert.java
@@ -1,14 +1,33 @@
 package MASWF.kreditantrag;
 
+import java.util.Date;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import MASWF.db.DAO.KreditDaoImpl;
+import MASWF.db.object.Kredit;
+import MASWF.db.object.Kunde;
 
 public class dbKreditInsert implements JavaDelegate {
 
 	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void execute(DelegateExecution exec) throws Exception {
+		Kredit kredit = new Kredit();
+		Kunde kunde = (Kunde) exec.getVariable("Kunde");
 
+		kredit.setKredithoehe((Long) exec.getVariable("wunschhoeheKredit"));
+		kredit.setKundenID(kunde.getKundeID());
+		kredit.setLaufzeit((Long) exec.getVariable("wunschlaufzeitKredit"));
+		kredit.setRueckzahlungrate((Float) exec.getVariable("rueckzahlungsrate"));
+		kredit.setStartdatum((Date) exec.getVariable("geburtsdatum"));
+		kredit.setVerwendungszweck(exec.getVariable("verwendungszweck").toString());
+		KreditDaoImpl kdao = new KreditDaoImpl();
+		if(kdao.insertKredit(kredit) == true){
+			System.out.println("Erfolgreich hinzugefügt");
+		} else {
+			System.out.println("ACHTUNG FEHLER DB");
+		}
 	}
 
 }
--- kreditantrag/src/main/java/MASWF/kreditantrag/erstelleKreditvertrag.java
+++ kreditantrag/src/main/java/MASWF/kreditantrag/erstelleKreditvertrag.java
@@ -1,28 +1,58 @@
 package MASWF.kreditantrag;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class erstelleKreditvertrag implements JavaDelegate {
-	private final String PDFLocation = "C:/Users/gelbe/git/MASBKS/kreditantrag/src/main/resources/formtest.pdf";
+	private final String PDFLocation = "C:/Users/gelbe/git/MASBKS/kreditantrag/src/main/resources/Kreditvertrag.pdf";
 	private final String PDFSpeichern = "C:/Users/gelbe/Desktop/formfilled.pdf";
+	private PDDocument pdfdoc;
+
 	@Override
 	public void execute(DelegateExecution exec) throws Exception {
-		PDDocument pdfdoc = PDDocument.load(new File(PDFLocation));
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		pdfdoc = PDDocument.load(new File(PDFLocation));
 		PDDocumentCatalog cat = pdfdoc.getDocumentCatalog();
 		PDAcroForm acro = cat.getAcroForm();
 		if (acro != null) {
-			PDField field = (PDField) acro.getField("vorname");
-			field.setValue(exec.getVariable("vorname").toString());
+			acro.getField("anrede").setValue(exec.getVariable("anrede").toString());
+			acro.getField("nachname").setValue(exec.getVariable("nachname").toString());
+
+			acro.getField("termin").setValue(df.format((Date) exec.getVariable("termin")));
+			acro.getField("kredithoehe").setValue(exec.getVariable("wunschhoeheKredit").toString());
+
+			acro.getField("laufzeit").setValue(exec.getVariable("wunschlaufzeitKredit").toString());
+
+			acro.getField("rueckzahlung").setValue(exec.getVariable("rueckzahlungsrate").toString());
+			// TexField...? mit formatf arbeiten...?
 		}
 		pdfdoc.save(new File(PDFSpeichern));
 		pdfdoc.close();
+		exec.setVariable("kreditvertrag", new File(PDFSpeichern));
 	}
-
+	// private void setField(String fieldname, String Inhalt){
+	// PDDocumentCatalog cat = pdfdoc.getDocumentCatalog();
+	// PDAcroForm acro = cat.getAcroForm();
+	// if (acro != null) {
+	// PDField field = (PDField) acro.getField(fieldname);
+	// try {
+	// field.setValue(Inhalt);
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	// }
 }
